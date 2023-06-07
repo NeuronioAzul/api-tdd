4c0e10d3749ec2583b6cb301e296def5814ebfab
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-07 12:44:14 -0300

    Added Tests: ⚗️ PHPUnit test files Clientes, Produtos, Pedidos

---
+++ tests/Feature/Api/ClientesControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\Clientes;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ClientesControllerTest extends TestCase
+{
+    /**
+     * Test get clientes endpoint
+     */
+    public function test_get_clientes_endpoint(): void
+    {
+        $clientes = Clientes::factory(3)->create();
+
+        $response = $this->getJson('/api/v1/clientes');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(5);
+    }
+}
