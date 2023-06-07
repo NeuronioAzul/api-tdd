cb5c02d1886afb36372e68d963308c6c3bb79a4e
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-06 21:06:25 -0300

    Modoify Routes: Added the Api v1 group route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\ClientesController;
+use App\Http\Controllers\Api\V1\PedidosController;
+use App\Http\Controllers\Api\V1\ProdutosController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,13 +10,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+
 });
