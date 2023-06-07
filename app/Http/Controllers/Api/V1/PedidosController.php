fb4cee6f3d9f09077c7d50eb77e0cf5a0786ba8e
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-07 20:24:07 -0300

    Added Feature: List Pedidos and associate the Cliente and Produto

--- app/Http/Controllers/Api/V1/PedidosController.php
+++ app/Http/Controllers/Api/V1/PedidosController.php
@@ -5,16 +5,46 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePedidosRequest;
 use App\Http\Requests\UpdatePedidosRequest;
+use App\Http\Resources\PedidosListResource;
+use App\Http\Resources\PedidosResource;
 use App\Models\Pedidos;
 
 class PedidosController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $query = Pedidos::query();
+        $pedidos = $query->paginate(5);
+        $pedidosListResource = PedidosListResource::collection($pedidos);
+
+        return response()->json(
+            [
+                'data' => $pedidosListResource,
+                'pagination' => [
+                    'links' =>
+                        [
+                            'first' => $pedidos->url(1),
+                            'last' => $pedidos->url($pedidos->lastPage()),
+                            'prev' => $pedidos->previousPageUrl(),
+                            'next' => $pedidos->nextPageUrl(),
+                        ],
+                    'meta' => [
+                        'current_page' => $pedidos->currentPage(),
+                        'from' => $pedidos->firstItem(),
+                        'last_page' => $pedidos->lastPage(),
+                        'path' => $pedidos->path(),
+                        'per_page' => $pedidos->perPage(),
+                        'to' => $pedidos->lastItem(),
+                        'total' => $pedidos->total(),
+                    ],
+                ],
+            ],
+            200
+        );
     }
 
     /**
@@ -47,5 +77,24 @@ class PedidosController extends Controller
     public function destroy(Pedidos $pedidos)
     {
         //
+    }
+
+
+    public function showProducts(Pedidos $pedidos)
+    {
+        $produtos = $pedidos->produtos;
+        return response()->json(
+            $produtos,
+            200
+        );
+    }
+
+    public function showCostumers(Pedidos $pedidos)
+    {
+        $clientes = $pedidos->clientes;
+        return response()->json(
+            $clientes,
+            200
+        );
     }
 }
