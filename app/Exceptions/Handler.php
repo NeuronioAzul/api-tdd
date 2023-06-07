28b900844733eb6217be593694960b9cf47591ab
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-06 23:44:58 -0300

    Modify Handler: Will handler the 404 error and will show the error message in Json format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -23,6 +24,13 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                "message" => "Opss! Resource not found",
+                "status" => 404
+            ], 404);
+        });
+
         $this->reportable(function (Throwable $e) {
             //
         });
