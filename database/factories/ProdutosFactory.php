56ba302a49df39ada332e3b37444588873f550f5
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-08 05:32:18 -0300

    Refactory The Factory: 🏭 now provides image file generation

--- database/factories/ProdutosFactory.php
+++ database/factories/ProdutosFactory.php
@@ -3,8 +3,11 @@
 namespace Database\Factories;
 
 use App\Models\Produtos;
+use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
+//use Illuminate\Http\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Produtos>
  */
@@ -19,11 +22,30 @@ class ProdutosFactory extends Factory
      */
     public function definition(): array
     {
-        $pastel = $this->faker->unique(false, 1000)->pastelName();
+//        $pastel = $this->faker->unique(false, 10000)->pastelName();
+//        return [
+//            'nome' => $pastel,
+//            'preco' => $this->faker->randomFloat(2, 3, 10),
+//            'foto' => $this->faker->imageUrl(600, 450, null, false, $pastel, true, 'png')
+//        ];
+//
+        $produtos_path = storage_path('app/public/produtos');
+
+        if (!File::exists($produtos_path)) {
+            File::makeDirectory($produtos_path);
+        }
+
+        $pastel = $this->faker->unique(false, 10000)->pastelName();
+
+        $image = $this->faker->image($produtos_path, 600, 450, null, false, true, $pastel, false );
+
         return [
             'nome' => $pastel,
             'preco' => $this->faker->randomFloat(2, 3, 10),
-            'foto' => $this->faker->imageUrl(600, 450, null, false, $pastel, true, 'png')
+            'foto' => $image
         ];
     }
+
+
+
 }
