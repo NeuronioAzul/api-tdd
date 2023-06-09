aa23731b635c3a584c8bec62fed91a9312488279
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-09 12:30:56 -0300

    Modify Factory: Produtos Right foto pub url

--- database/factories/ProdutosFactory.php
+++ database/factories/ProdutosFactory.php
@@ -3,11 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\Produtos;
-use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 //use Illuminate\Http\File;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Produtos>
  */
@@ -22,30 +21,16 @@ class ProdutosFactory extends Factory
      */
     public function definition(): array
     {
-//        $pastel = $this->faker->unique(false, 10000)->pastelName();
-//        return [
-//            'nome' => $pastel,
-//            'preco' => $this->faker->randomFloat(2, 3, 10),
-//            'foto' => $this->faker->imageUrl(600, 450, null, false, $pastel, true, 'png')
-//        ];
-//
         $produtos_path = storage_path('app/public/produtos');
 
-        if (!File::exists($produtos_path)) {
-            File::makeDirectory($produtos_path);
-        }
+        $pastel = $this->faker->unique(false, 1000)->pastelName();
 
-        $pastel = $this->faker->unique(false, 10000)->pastelName();
-
-        $image = $this->faker->image($produtos_path, 600, 450, null, false, true, $pastel, false );
+        $image = $this->faker->image($produtos_path, 600, 450, null, false, true, $pastel, false);
 
         return [
             'nome' => $pastel,
             'preco' => $this->faker->randomFloat(2, 3, 10),
-            'foto' => $image
+            'foto' => 'public/produtos/'.$image
         ];
     }
-
-
-
 }
