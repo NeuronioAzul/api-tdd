4d38951a0ea9ed3b0f558a7e11e1e706c99d3a91
Mauro R T <mauro.rocha.t@gmail.com>
2023-06-09 12:29:16 -0300

    Modify Seeder: Produtos clean files before create

--- database/seeders/ProdutosSeeder.php
+++ database/seeders/ProdutosSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Produtos;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class ProdutosSeeder extends Seeder
 {
@@ -13,8 +14,16 @@ class ProdutosSeeder extends Seeder
      */
     public function run(): void
     {
+        $directory = storage_path('app/public/produtos');
+
+        if (File::isDirectory($directory)) {
+            File::cleanDirectory($directory);
+        } else {
+            File::makeDirectory($directory, 0755, true);
+        }
+
         Produtos::factory()
-            ->count(50)
+            ->count(10)
             ->create();
     }
 }
